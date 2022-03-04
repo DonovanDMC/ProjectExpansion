0eb54f1191489f70cfeaecd51548626ecfc63a87
Donovan Daniels <hewwo@yiff.rocks>
2022-03-04 14:24:43 -0600

    1.0.10

--- scripts/gen-release.sh
+++ scripts/gen-release.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+DIR=$(realpath "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )")
+cd $DIR/publish
 # ESM & I can't be bothered to transpile
-node --experimental-specifier-resolution=node  --loader ts-node/esm js/run.ts
+node --experimental-specifier-resolution=node --loader ts-node/esm run.ts
