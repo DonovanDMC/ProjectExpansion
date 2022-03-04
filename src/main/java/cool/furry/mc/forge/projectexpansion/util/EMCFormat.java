f83f1ed92afeb26c667ffc258d038827f923d275
Donovan Daniels <hewwo@yiff.rocks>
2022-03-04 14:19:41 -0600

    shorten numbers in EMCFormat

--- src/main/java/cool/furry/mc/forge/projectexpansion/util/EMCFormat.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/util/EMCFormat.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 
 public class EMCFormat extends DecimalFormat {
     public static EMCFormat INSTANCE = new EMCFormat();
+    public static final double FORMAT_START = 1e6;
 
     private EMCFormat() {
         super("#,###");
@@ -18,31 +19,30 @@ public class EMCFormat extends DecimalFormat {
     }
 
     private static final Object[][] list = {
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "No", "Novemdecillion" }, // 10^60
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "Oc", "Octodecillion" }, // 10^57
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "St", "Septendecillion" }, // 10^54
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "Sd", "Sexdecillion" }, // 10^51
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "Qd", "Quindecillion" }, // 10^48
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "Qt", "Quattuordecillion" }, // 10^45
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000D, "Tr", "Tredecillion" }, // 10^42
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000_000D, "Du", "Duodecillion" }, // 10^39
-            { 1_000_000_000_000_000_000_000_000_000_000_000_000D, "U", "Undecillion" }, // 10^36
-            { 1_000_000_000_000_000_000_000_000_000_000_000D, "D", "Decillion" }, // 10^33
-            { 1_000_000_000_000_000_000_000_000_000_000D, "N", "Nonillion" }, // 10^30
-            { 1_000_000_000_000_000_000_000_000_000D, "O", "Octillion" }, // 10^27
-            { 1_000_000_000_000_000_000_000_000D, "Sp", "Septillion" }, // 10^24
-            { 1_000_000_000_000_000_000_000D, "Sx", "Sextillion" }, // 10^21
-            { 1_000_000_000_000_000_000D, "Qu", "Quintillion" }, // 10^18
-            { 1_000_000_000_000_000D, "Qa", "Quadrillion" }, // 10^15
-            { 1_000_000_000_000D, "T", "Trillion" }, // 10^12
-            { 1_000_000_000D, "B", "Billion" }, // 10^9
-            { 1_000_000D, "M", "Million" }, // 10^6
+            { 1e60, "No", "Novemdecillion" },
+            { 1e57, "Oc", "Octodecillion" },
+            { 1e54, "St", "Septendecillion" },
+            { 1e51, "Sd", "Sexdecillion" },
+            { 1e48, "Qd", "Quindecillion" },
+            { 1e45, "Qt", "Quattuordecillion" },
+            { 1e42, "Tr", "Tredecillion" },
+            { 1e39, "Du", "Duodecillion" },
+            { 1e36, "U", "Undecillion" },
+            { 1e33, "D", "Decillion" },
+            { 1e30, "N", "Nonillion" },
+            { 1e27, "O", "Octillion" },
+            { 1e24, "Sp", "Septillion" },
+            { 1e21, "Sx", "Sextillion" },
+            { 1e18, "Qu", "Quintillion" },
+            { 1e15, "Qa", "Quadrillion" },
+            { 1e12, "T", "Trillion" },
+            { 1e9, "B", "Billion" },
+            { 1e6, "M", "Million" },
     };
 
     @Override
     public StringBuffer format(double number, StringBuffer result, FieldPosition fieldPosition) {
-        // TransmutationEMCFormatter.formatEMC(emcAmount)
-        if(Config.formatEMC.get() && number > 1_000_000D && !Screen.hasShiftDown()) {
+        if(Config.formatEMC.get() && number > FORMAT_START && !Screen.hasShiftDown()) {
             Object[] res = Arrays.stream(list).filter((p) -> number >= (double) p[0]).findFirst().orElse(new Object[]{ number, "", "" });
             double num = number;
             @Nullable
