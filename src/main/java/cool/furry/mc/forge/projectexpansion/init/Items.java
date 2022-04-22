3a3e902dc839e9647d068645727d8f908021cb70
Donovan Daniels <hewwo@yiff.rocks>
2022-04-21 23:47:38 -0500

    Backport f99ed1d21443c9418d846d1c72ebe085506e40ea & c8d520830f653998f34c2933e32024f65f36f93d

--- src/main/java/cool/furry/mc/forge/projectexpansion/init/Items.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/init/Items.java
@@ -1,7 +1,10 @@
 package cool.furry.mc.forge.projectexpansion.init;
 
 import cool.furry.mc.forge.projectexpansion.Main;
-import cool.furry.mc.forge.projectexpansion.item.*;
+import cool.furry.mc.forge.projectexpansion.item.ItemFinalStar;
+import cool.furry.mc.forge.projectexpansion.item.ItemInfiniteFuel;
+import cool.furry.mc.forge.projectexpansion.item.ItemInfiniteSteak;
+import cool.furry.mc.forge.projectexpansion.item.ItemUpgrade;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Rarity;
@@ -12,8 +15,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class Items {
     public static final DeferredRegister<Item> Registry = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
 
-    public static final RegistryObject<Item> ARCANE_TABLET = Registry.register("arcane_tablet", ItemArcaneTablet::new);
-    public static final RegistryObject<BlockItem> ARCANE_TABLE = Registry.register("arcane_table", () -> new BlockItem(Blocks.ARCANE_TABLE.get(), new Item.Properties().group(Main.group).rarity(Rarity.RARE)));
     public static final RegistryObject<Item> FINAL_STAR_SHARD = Registry.register("final_star_shard", () -> new Item(new Item.Properties().maxStackSize(1).rarity(Rarity.RARE).group(Main.group)));
     public static final RegistryObject<Item> FINAL_STAR = Registry.register("final_star", ItemFinalStar::new);
     public static final RegistryObject<Item> MATTER_UPGRADER = Registry.register("matter_upgrader", ItemUpgrade::new);
