f99ed1d21443c9418d846d1c72ebe085506e40ea
Donovan Daniels <hewwo@yiff.rocks>
2022-04-21 22:45:33 -0500

    remove arcane table/tablet They will be added back once we have a working prototype, see #22

--- src/main/java/cool/furry/mc/forge/projectexpansion/init/Blocks.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/init/Blocks.java
@@ -1,7 +1,6 @@
 package cool.furry.mc.forge.projectexpansion.init;
 
 import cool.furry.mc.forge.projectexpansion.Main;
-import cool.furry.mc.forge.projectexpansion.block.BlockArcaneTable;
 import cool.furry.mc.forge.projectexpansion.block.BlockTransmutationInterface;
 import net.minecraft.block.Block;
 import net.minecraftforge.fml.RegistryObject;
@@ -11,6 +10,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class Blocks {
     public static final DeferredRegister<Block> Registry = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
 
-    public static final RegistryObject<BlockArcaneTable> ARCANE_TABLE = Registry.register("arcane_table", BlockArcaneTable::new);
     public static final RegistryObject<BlockTransmutationInterface> TRANSMUTATION_INTERFACE = Registry.register("transmutation_interface", BlockTransmutationInterface::new);
 }
