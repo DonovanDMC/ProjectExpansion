0e81005bbbf1b8268feee1827dff0ea735395085
Kithio <9414786+Kithio@users.noreply.github.com>
2022-04-28 15:36:36 +0200

    Adjust EMC Link Recipe + Script
    Adjust recipe to use Matter
    Adjust script to programatically write all recipe files, no more use for single recipes

--- src/main/generation/generators/recipes/emc_link.js
+++ src/main/generation/generators/recipes/emc_link.js
@@ -3,11 +3,33 @@ const { MATTER_TIERS } = require("../../util");
 
 const BASE = `${__dirname}/emc_link.json`;
 module.exports = function run(outDir) {
-    if(!existsSync(`${outDir}/emc_link`)) mkdirSync(`${outDir}/emc_link`);
+    if (!existsSync(`${outDir}/emc_link`)) mkdirSync(`${outDir}/emc_link`);
     const base = readFileSync(BASE).toString();
-    return MATTER_TIERS.filter(tier => !["basic", "dark", "red", "final"].includes(tier)).map((tier, index, arr) => {
-        if(tier === "magenta") return; // we still need it for pink
-        writeFileSync(`${outDir}/emc_link/${tier}.json`, base.replace(/\$TIER\$/g, tier).replace(/\$PREV\$/g, arr[index - 1]));
+    return MATTER_TIERS.map((tier, index, arr) => {
+        writeFileSync(`${outDir}/emc_link/${tier}.json`, base.replace(/\$TARGET\$/g, getMatter(tier)).replace(/\$CORE\$/g, getCore(tier)).replace(/\$TIER\$/g, tier).replace(/\$PREV\$/g, arr[index - 1]));
         return [BASE, `${outDir}/emc_link/${tier}.json`];
     }).filter(Boolean);
+}
+
+function getCore(tier) {
+    switch (tier) {
+        case MATTER_TIERS[0]:
+            return "projecte:condenser_mk1";
+        default:
+            return "projectexpansion:$PREV$_emc_link";
+    }
+}
+
+function getMatter(tier) {
+    switch (tier) {
+        case MATTER_TIERS[0]:
+            return "projecte:transmutation_tablet";
+        case MATTER_TIERS[1]:
+        case MATTER_TIERS[2]:
+            return "projecte:$TIER$_matter";
+        case MATTER_TIERS[15]:
+            return "projectexpansion:final_star_shard"
+        default:
+            return "projectexpansion:$TIER$_matter";
+    }
 }
