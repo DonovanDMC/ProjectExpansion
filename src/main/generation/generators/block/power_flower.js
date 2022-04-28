0f9a1f8d2b0e5dbdb05301d0c4c2a6e746296a3c
Donovan Daniels <hewwo@yiff.rocks>
2022-04-28 10:41:08 -0500

    formatting

--- src/main/generation/generators/block/power_flower.js
+++ src/main/generation/generators/block/power_flower.js
@@ -3,7 +3,7 @@ const {genericBlock} = require("../../util");
 
 const BASE = `${__dirname}/power_flower.json`;
 module.exports = function run(outDir) {
-    if (!existsSync(`${outDir}/power_flower`)) mkdirSync(`${outDir}/power_flower`);
-    copyFileSync(`${__dirname}/single/power_flower_base.json`, `${outDir}/power_flower/base.json`);
-    return genericBlock(outDir, "power_flower", BASE);
+	if (!existsSync(`${outDir}/power_flower`)) mkdirSync(`${outDir}/power_flower`);
+	copyFileSync(`${__dirname}/single/power_flower_base.json`, `${outDir}/power_flower/base.json`);
+	return genericBlock(outDir, "power_flower", BASE);
 };
--- src/main/generation/generators/recipes/power_flower.js
+++ src/main/generation/generators/recipes/power_flower.js
@@ -3,10 +3,10 @@ const {MATTER_TIERS} = require("../../util");
 
 const BASE = `${__dirname}/power_flower.json`;
 module.exports = function run(outDir) {
-    if (!existsSync(`${outDir}/power_flower`)) mkdirSync(`${outDir}/power_flower`);
-    const base = readFileSync(BASE).toString();
-    return MATTER_TIERS.map((tier, index, arr) => {
-        writeFileSync(`${outDir}/power_flower/${tier}.json`, base.replace(/\$TIER\$/g, tier));
-        return [BASE, `${outDir}/power_flower/${tier}.json`];
-    });
+	if (!existsSync(`${outDir}/power_flower`)) mkdirSync(`${outDir}/power_flower`);
+	const base = readFileSync(BASE).toString();
+	return MATTER_TIERS.map((tier, index, arr) => {
+		writeFileSync(`${outDir}/power_flower/${tier}.json`, base.replace(/\$TIER\$/g, tier));
+		return [BASE, `${outDir}/power_flower/${tier}.json`];
+	});
 }
