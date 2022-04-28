39698d0de9e9f402b9a4a72b7cededdb3674ae50
Kithio <Kithio.Lost@gmail.com>
2022-04-28 06:51:45 +0200

    Fixed Oversight in EMCLink [1.14]

--- src/main/java/cool/furry/mc/forge/projectexpansion/tile/TileEMCLink.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/tile/TileEMCLink.java
@@ -105,7 +105,8 @@ public class TileEMCLink extends TileEntity implements ITickableTileEntity, IEmc
     }
 
     private void setInternalItem(ItemStack stack) {
-        itemStack = stack;
+        itemStack = stack.copy();
+        itemStack.setCount(1);
         markDirty();
     }
 
