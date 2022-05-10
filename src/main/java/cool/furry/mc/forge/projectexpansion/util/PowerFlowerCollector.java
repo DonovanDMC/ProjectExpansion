7bc38b452d1b87ab6c9b97eeb2c1fc6a98bb515c
Donovan Daniels <hewwo@yiff.rocks>
2022-05-09 20:06:51 -0500

    fix ConcurrentModificationException

--- src/main/java/cool/furry/mc/forge/projectexpansion/util/PowerFlowerCollector.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/util/PowerFlowerCollector.java
@@ -10,9 +10,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class PowerFlowerCollector {
@@ -25,19 +23,17 @@ public class PowerFlowerCollector {
     @SubscribeEvent
     public static void onTick(TickEvent.ServerTickEvent event) {
         tick++;
-        if (tick >= (Config.tickDelay.get() + 3)) {
-            tick = 0;
-            for(UUID uuid : stored.keySet()) {
-                BigInteger amount = stored.get(uuid);
-                ServerPlayerEntity player = Util.getPlayer(uuid);
-                if (player == null)
-                    continue;
-                IKnowledgeProvider provider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(uuid);
-                provider.setEmc(provider.getEmc().add(amount));
-                provider.sync(player);
-                stored.remove(uuid);
-            }
+        Set<UUID> toRemove = new HashSet<>();
+        for(UUID uuid : stored.keySet()) {
+            BigInteger amount = stored.get(uuid);
+            ServerPlayerEntity player = Util.getPlayer(uuid);
+            if (player == null) continue;
+            IKnowledgeProvider provider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(uuid);
+            provider.setEmc(provider.getEmc().add(amount));
+            provider.sync(player);
+            toRemove.add(uuid);
         }
+        toRemove.forEach(stored::remove);
     }
 }
 
