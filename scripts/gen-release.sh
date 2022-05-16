a16f4d29dd86ff2339775692b6a649dd3e24046d
Donovan Daniels <hewwo@yiff.rocks>
2022-05-16 04:01:11 -0500

    more multiplier config options

--- scripts/gen-release.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DIR=$(realpath "$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )")
-cd $DIR/publish
-# ESM & I can't be bothered to transpile
-node --experimental-specifier-resolution=node --loader ts-node/esm run.ts
