9905b7f31a2fd3b7cd307b0dcff1c9a62d4e9ab1
Donovan Daniels <hewwo@yiff.rocks>
2022-05-23 08:17:06 -0500

    make emc display more consistent

--- src/main/java/cool/furry/mc/forge/projectexpansion/gui/EMCDisplay.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/gui/EMCDisplay.java
@@ -4,7 +4,6 @@ import cool.furry.mc.forge.projectexpansion.Main;
 import cool.furry.mc.forge.projectexpansion.config.Config;
 import cool.furry.mc.forge.projectexpansion.util.EMCFormat;
 import moze_intel.projecte.api.ProjectEAPI;
-import moze_intel.projecte.api.capabilities.IKnowledgeProvider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.util.text.TextFormatting;
@@ -13,18 +12,19 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 import java.math.BigInteger;
-import java.util.Arrays;
 
 @OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class EMCDisplay {
-    private static BigInteger change = BigInteger.ZERO;
     private static BigInteger emc = BigInteger.ZERO;
+    private static final BigInteger[] history = new BigInteger[]{BigInteger.ZERO, BigInteger.ZERO};
+    private static BigInteger lastEMC = BigInteger.ZERO;
     private static int tick = 0;
 
     private static @Nullable
@@ -40,28 +40,37 @@ public class EMCDisplay {
         if (event.phase == TickEvent.Phase.END && player != null && tick >= 20) {
             tick = 0;
             player.getCapability(ProjectEAPI.KNOWLEDGE_CAPABILITY).ifPresent((provider) -> {
-                BigInteger lastEMC = emc;
                 emc = provider.getEmc();
-                change = emc.subtract(lastEMC);
+                history[1] = history[0];
+                history[0] = emc.subtract(lastEMC);
+                lastEMC = emc;
             });
         }
     }
 
-    @SubscribeEvent
-    public static void clientDisconnect(ClientPlayerNetworkEvent.LoggedOutEvent event) {
-        if (!Config.emcDisplay.get())
-            return;
-        emc = change = BigInteger.ZERO;
+    private static void reset() {
+        emc = lastEMC = BigInteger.ZERO;
         tick = 0;
     }
 
     @SubscribeEvent
+    public static void clientDisconnect(ClientPlayerNetworkEvent.LoggedOutEvent event) {
+        if (!Config.emcDisplay.get()) return;
+        reset();
+    }
+
+    @SubscribeEvent
+    public static void onWorldUnload(WorldEvent.Unload event) {
+        if (!Config.emcDisplay.get()) return;
+        reset();
+    }
+
+    @SubscribeEvent
     public static void onRenderGUI(RenderGameOverlayEvent.Text event) {
-        if (!Config.emcDisplay.get())
-            return;
-        String str = EMCFormat.INSTANCE.format(emc.doubleValue());
-        if (!change.equals(BigInteger.ZERO))
-            str += " " + (change.compareTo(BigInteger.ZERO) > 0 ? (TextFormatting.GREEN + "+") : (TextFormatting.RED + "-")) + EMCFormat.INSTANCE.format(Math.abs(change.doubleValue())) + "/s";
+        if (!Config.emcDisplay.get()) return;
+        BigInteger avg = history[0].add(history[1]);
+        String str = EMCFormat.format(emc);
+        if (!avg.equals(BigInteger.ZERO)) str += " " + (avg.compareTo(BigInteger.ZERO) > 0 ? (TextFormatting.GREEN + "+") : (TextFormatting.RED + "-")) + EMCFormat.format(avg.abs()) + "/s";
         event.getLeft().add(String.format("EMC: %s", str));
     }
 }
