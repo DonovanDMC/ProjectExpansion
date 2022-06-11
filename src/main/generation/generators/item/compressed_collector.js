8a202bd4679152b1873465114afa71a39c5acd0f
Donovan Daniels <hewwo@yiff.rocks>
2022-06-10 20:39:34 -0500

    generation submodule

--- src/main/generation/generators/item/compressed_collector.js
+++
@@ -1,4 +0,0 @@
-const {generic} = require("../../util");
-
-const BASE = `${__dirname}/collector.json`;
-module.exports = (outDir) => generic(outDir, "compressed_collector", BASE);
--- src/main/generation/generators/item/fuel.js
+++
@@ -1,4 +0,0 @@
-const {generic, FUEL_DISABLED} = require("../../util");
-
-const BASE = `${__dirname}/fuel.json`;
-module.exports = (outDir) => generic(outDir, "fuel", BASE, FUEL_DISABLED);
