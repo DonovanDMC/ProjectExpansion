b1fa7b16c4369e49cb8751c8bf6983ceedcd1276
Donovan Daniels <hewwo@yiff.rocks>
2022-07-18 13:13:34 -0500

    Infinite Fuel & Steak Config Options (fixes #64)

--- src/main/java/cool/furry/mc/forge/projectexpansion/item/ItemInfiniteFuel.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/item/ItemInfiniteFuel.java
@@ -1,6 +1,7 @@
 package cool.furry.mc.forge.projectexpansion.item;
 
 import cool.furry.mc.forge.projectexpansion.Main;
+import cool.furry.mc.forge.projectexpansion.config.Config;
 import cool.furry.mc.forge.projectexpansion.util.ColorStyle;
 import cool.furry.mc.forge.projectexpansion.util.EMCFormat;
 import moze_intel.projecte.api.ProjectEAPI;
@@ -9,7 +10,6 @@ import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.util.text.ITextComponent;
@@ -23,11 +23,8 @@ import javax.annotation.Nullable;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.UUID;
-import java.util.function.Supplier;
 
 public class ItemInfiniteFuel extends Item {
-    public static final Supplier<BigInteger> COST = () -> BigInteger.valueOf(ProjectEAPI.getEMCProxy().getValue(Items.COAL));
-    public static final int BURN_TIME = 1600;
 
     public ItemInfiniteFuel() {
         super(new Item.Properties().maxStackSize(1).rarity(Rarity.RARE).group(Main.group));
@@ -38,14 +35,14 @@ public class ItemInfiniteFuel extends Item {
     public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> list, ITooltipFlag flag) {
         super.addInformation(stack, world, list, flag);
         list.add(new TranslationTextComponent("item.projectexpansion.infinite_fuel.tooltip").setStyle(ColorStyle.GRAY));
-        list.add(new TranslationTextComponent("text.projectexpansion.cost", EMCFormat.getComponent(BURN_TIME).setStyle(ColorStyle.GRAY)).setStyle(ColorStyle.RED));
+        list.add(new TranslationTextComponent("text.projectexpansion.cost", EMCFormat.getComponent(Config.infiniteFuelCost.get()).setStyle(ColorStyle.GRAY)).setStyle(ColorStyle.RED));
     }
 
     @Override
     public int getBurnTime(ItemStack stack, @Nullable IRecipeType<?> recipeType) {
         @Nullable UUID owner = stack.getTag() == null ? null : stack.getTag().getUniqueId("Owner");
         if (owner == null) return 0;
-        return ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner).getEmc().compareTo(COST.get()) < 0 ? 0 : BURN_TIME;
+        return (Config.infiniteFuelCost.get() == 0 || Config.infiniteFuelBurnTime.get() == 0) ? 0 : ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner).getEmc().compareTo(BigInteger.valueOf(Config.infiniteFuelCost.get())) < 0 ? 0 : Config.infiniteFuelBurnTime.get();
     }
 
     @Override
@@ -59,7 +56,7 @@ public class ItemInfiniteFuel extends Item {
         if (owner == null) return stack;
         ServerPlayerEntity player = ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayerByUUID(owner);
         IKnowledgeProvider provider = ProjectEAPI.getTransmutationProxy().getKnowledgeProviderFor(owner);
-        provider.setEmc(provider.getEmc().subtract(COST.get()));
+        provider.setEmc(provider.getEmc().subtract(BigInteger.valueOf(Config.infiniteFuelCost.get())));
         if (player != null) provider.syncEmc(player);
         return stack;
     }
