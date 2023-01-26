bb8ac14c11ecfebdb74380a4240d92dfa7ba897a
Donovan Daniels <hewwo@yiff.rocks>
2023-01-26 02:39:15 -0600

    Add `persistEnchantedBooksOnly` config option

--- src/main/java/cool/furry/mc/forge/projectexpansion/config/Config.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/config/Config.java
@@ -24,5 +24,6 @@ public final class Config {
     public static final ForgeConfigSpec.ConfigValue<Integer> infiniteFuelCost = Builder.comment("The cost of using the infinite fuel item.").defineInRange("infiniteFuelCost", 128, 1, Integer.MAX_VALUE);
     public static final ForgeConfigSpec.ConfigValue<Integer> infiniteFuelBurnTime = Builder.comment("The ticks each usage of the infinite fuel item will give.").defineInRange("infiniteFuelBurnTime", 1600, 1, Integer.MAX_VALUE);
     public static final ForgeConfigSpec.ConfigValue<Integer> infiniteSteakCost = Builder.comment("The cost of using the infinite steak item.").defineInRange("infiniteSteakCost", 64, 1, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.ConfigValue<Boolean> persistEnchantedBooksOnly = Builder.comment("If ProjectE's processors.EnchantmentProcessor.persistent option should only include enchanted books.").define("persistEnchantedBooksOnly", false);
     static { Spec = Builder.build(); }
 }
