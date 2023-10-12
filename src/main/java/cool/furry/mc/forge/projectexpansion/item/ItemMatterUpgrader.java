d9acd1b44d82e9b595f2866647fd16215a510efb
Donovan Daniels <hewwo@yiff.rocks>
2023-10-12 12:18:59 -0500

    Fix matter upgrader failing after world reload

--- src/main/java/cool/furry/mc/forge/projectexpansion/item/ItemMatterUpgrader.java
+++ src/main/java/cool/furry/mc/forge/projectexpansion/item/ItemMatterUpgrader.java
@@ -88,7 +88,7 @@ public class ItemMatterUpgrader extends Item {
             upgrade = Objects.requireNonNull(upgradeTo.getPowerFlowerItem());
             upgradeBlock = Objects.requireNonNull(upgradeTo.getPowerFlower());
             if (be.owner == null) return InteractionResult.FAIL;
-            if (be.owner != player.getUUID()) {
+            if (!be.owner.equals(player.getUUID())) {
                 player.displayClientMessage(Lang.Items.MATTER_UPGRADER_NOT_OWNER.translateColored(ChatFormatting.RED), true);
                 return InteractionResult.FAIL;
             }
